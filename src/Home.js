4042f535e465efa5851569758a81033041a6398c
Shalini-Vetri1205 <selvanvetri076@gmail.com>
2024-03-11 04:05:32 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://img.freepik.com/free-psd/clean-modern-resume-portfolio-cv-template_120329-3615.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709683200&semt=ais" alt="Welcome picture" />
+    </div>
+  )
+}
